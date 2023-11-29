a0938a57d931bb55474e69a0a7a49cbde75eaaec
fabien <fabienpeigne@gmail.com>
2023-11-29 17:42:22 +0100

    last versions before push

--- features_extraction/features_computation.py
+++ features_extraction/features_computation.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 # import scipy.signal
 import features_functions as ff
+import argparse
 
 # preprocessing and pandas stuff
 def preprocess_datas_timeline(datas):
@@ -165,3 +166,37 @@ def get_dataframe_features(datas,window_size_sec,fs,list_features_type):
         df_features = df_features.append(all_features,ignore_index=True)
     
     return(df_features)
+
+##############################################################Parsing and extraction #######################################################################################################
+
+default_list_features_type = ['temporal_features','frequential_features','multiple_parameters_frequential_features','fourier_spectrum_features']
+dir_datas = '../datas/'
+datas_test = 'datas_test_feature_extraction.csv'
+
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("window_size_sec", help="the window size in second on which you calculate your features",type=int)
+parser.add_argument("datas_path",nargs='?', const = 1, default = dir_datas + datas_test, help="absolute or relaive path to your datas",type = str)
+parser.add_argument("features_type", nargs='?', const = 1, default = 'all',help="list of features type, default = all")
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    
+    window_size_sec = args.window_size_sec
+    datas_path = args.datas_path
+
+    if args.features_type == 'all':
+        list_features_type = default_list_features_type
+    else:
+        list_features_type = args.features_type
+
+    datas = pd.read_csv(datas_path,index_col = 0)
+    fs = 45
+    df = get_dataframe_features(datas,window_size_sec,fs,list_features_type)
+    print('nb lignes features calculatesd = ', np.shape(df)[0])
+    print('nb features per lignes = ', np.shape(df)[1])  
+
+    df.to_csv(dir_datas + 'features_{}_ws_{}.csv'.format(datas_test,window_size_sec))
